6bdf7f3567908552e63a12bba2dcb22b55dd7c9f
Keqiu Hu <khu@linkedin.com>
2018-05-29 22:00:47 -0700

    Initial Commit

---
+++ ShadowsocksX-dummy/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  ShadowsocksX-dummy
+//
+//  Created by Keqiu Hu on 5/29/18.
+//  Copyright © 2018 Keqiu Hu. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ ShadowsocksX-dummy/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  ShadowsocksX-dummy
+//
+//  Created by Keqiu Hu on 5/29/18.
+//  Copyright © 2018 Keqiu Hu. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
